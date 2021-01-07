5f4903535b45bd2be2ae731f6c74c059cd4f4bdc
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-01-07 13:57:27 +0100

    Comment out lines unnecessary for MVP

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const grid = document.querySelector(".grid");
 grid.addEventListener("click", (e) => flipCard(e));
 
 function flipCard(e) {
-  console.log(e.target.parentNode.classList);
   if (e.target.parentNode.classList.contains("grid__card__inner")) {
     e.target.parentNode.classList.toggle("grid__card__inner--flipped");
   }
